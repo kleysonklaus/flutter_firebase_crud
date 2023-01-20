8651e1d20b9d3f05cc31c429aff0fb6dbf01dd1e
kleyson klaus <kleyson.zur44@gmail.com>
2023-01-19 22:58:18 -0500

    feat(edit_name_page): implement to update a name

--- lib/screens/edit_name_page.dart
+++ lib/screens/edit_name_page.dart
@@ -12,25 +12,37 @@ class _EditNamePageState extends State<EditNamePage> {
   final controller = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Map args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+    controller.text = args['name'];
     return Scaffold(
-      appBar: AppBar(title: const Text("añadir")),
+      appBar: AppBar(title: const Text("Editar nombre")),
       body: Column(
         children: [
           TextField(
             controller: controller,
-            decoration: InputDecoration(
-              hintText: 'Ingrese nuevo nombre',
+            decoration: const InputDecoration(
+              hintText: 'Editar nombre',
             ),
           ),
           ElevatedButton(
             onPressed: () async {
               if (controller.text.isEmpty) return;
-              await insertName(name: controller.text).then(
+              await updateName(
+                uid: args['uid'],
+                newName: controller.text,
+              ).then(
                 (value) => Navigator.of(context).pop(),
               );
             },
-            child: const Text("Guardar"),
+            child: const Text("Actualizar"),
           ),
         ],
       ),
