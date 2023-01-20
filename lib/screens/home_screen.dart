1c7cb7bbbe988cef0b434f1648f6e20ddb2f3cdc
kleyson klaus <kleyson.zur44@gmail.com>
2023-01-19 22:41:03 -0500

    feat(home_screen): add parameter arguments name

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -36,7 +36,7 @@ class _HomeState extends State<Home> {
                   Navigator.pushNamed(
                     context,
                     '/edit',
-                    arguments: item,
+                    arguments: item['name'],
                   );
                 },
               );
